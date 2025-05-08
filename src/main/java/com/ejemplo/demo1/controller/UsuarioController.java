8a1aba3ae4a32367ebc6abcff357cffb3583c95a
Isai Reyes <161645019+isairey@users.noreply.github.com>
2025-05-08 10:37:41 -0600

    Update UsuarioController.java

--- src/main/java/com/ejemplo/demo1/controller/UsuarioController.java
+++ src/main/java/com/ejemplo/demo1/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 
 
 import java.util.List;
-@CrossOrigin(origins = "https://main--soft-torrone-90dd2d.netlify.app") 
+@CrossOrigin(origins = "https://soft-torrone-90dd2d.netlify.app") 
 
 
 @RestController
