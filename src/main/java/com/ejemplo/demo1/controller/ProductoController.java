5179b71453ea4c5ebf62ef15b196ea4c6c360676
Isai Reyes <161645019+isairey@users.noreply.github.com>
2025-05-08 10:38:15 -0600

    Update ProductoController.java

--- src/main/java/com/ejemplo/demo1/controller/ProductoController.java
+++ src/main/java/com/ejemplo/demo1/controller/ProductoController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.*;
 import java.util.List;
+@CrossOrigin(origins = "https://soft-torrone-90dd2d.netlify.app") 
 
 @RestController
 @RequestMapping("/producto")
